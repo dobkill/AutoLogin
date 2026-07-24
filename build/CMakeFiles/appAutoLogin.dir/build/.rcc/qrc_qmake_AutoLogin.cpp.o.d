5a2b96f6890b1e47219863397c1c6a02ecce4d90
dobkill <doublekill@139.com>
2026-07-24 22:10:11 +0800

    igore

--- build/CMakeFiles/appAutoLogin.dir/build/.rcc/qrc_qmake_AutoLogin.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/appAutoLogin.dir/build/.rcc/qrc_qmake_AutoLogin.cpp.o: \
- /home/a/Yuan/Code/QT_learn/AutoLogin/build/.rcc/qrc_qmake_AutoLogin.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/appAutoLogin_tooling.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for appAutoLogin_tooling.
-# This may be replaced when dependencies are built.
